6bd28ef33ba2b06b62547f8824d05c888dfe1dcd
Sams-Ddev <sams.alxstudent@gmail.com>
2023-06-17 10:00:48 -0700

    prints whether the variable store in is - or +

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - will assign a random number to the variable and
+ * each time it is executed
+ * and determines if each random number is
+ * _potive or _negative integer
+ *
+ * Return: 0 if number is negative.1 if n is positive
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+	{
+		printf("n = %d is Negative\n", n);
+	}
+	else
+	{
+		printf("n = %d is Positive\n", n);
+	}
+	return (0);
+}
