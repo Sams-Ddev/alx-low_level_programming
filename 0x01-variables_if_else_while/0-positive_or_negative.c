e1bcd15729586e6ad2930e7962356c7b595397b9
Sams-Ddev <sams.alxstudent@gmail.com>
2023-06-17 10:34:28 -0700

    correct printf message

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,15 +18,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
 	{
-		printf("n = %d is Negative\n", n);
+		printf("%d is Negative\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("n = %d is Zero\n", n);
+		printf("%d is Zero\n", n);
 	}
 	else
 	{
-		printf("n = %d is Positive\n", n);
+		printf("%d is Positive\n", n);
 	}
 	return (0);
 }
