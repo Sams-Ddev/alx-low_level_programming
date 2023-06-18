842b753c284f50c73ace75da70e174a658c9c4fe
Sams-Ddev <sams.alxstudent@gmail.com>
2023-06-17 18:25:23 -0700

    modulus remainder

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,19 +16,21 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	int last_dig = n % 10;
-	
+	last_dig = n % 10;
+
 	if (last_dig > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5", n, last_dig);
 	}
 	else if (last_dig == 0)
 	{
-		printf("Last digits of %d is %d and is 0", n, last_dig );
+		printf("Last digits of %d is %d and is 0", n, last_dig);
 	}
 	else if (last_dig < 6 && n != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0", n, last_dig);
 	}
+	printf("\n");
+
 	return (0);
 }
