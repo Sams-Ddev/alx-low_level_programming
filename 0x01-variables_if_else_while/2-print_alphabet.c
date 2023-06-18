2a02b5413c5c7d4f72c524ae4cb9b73940ac0840
Sams-Ddev <sams.alxstudent@gmail.com>
2023-06-17 19:18:55 -0700

    use of putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints a lphabets to z using
+ * putchar twice
+ *
+ * Return: always(0)
+ */
+
+int main(void)
+{
+	char alphbet;
+
+	for (alphbet = 'a'; alphbet <= 'z'; alphbet++)
+	{
+		putchar(alphbet);
+		putchar('\n');
+	}
+
+	return (0);
+}
