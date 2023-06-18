0bd4780d7e9a0367295d52435423b9ea917e0b27
Sams-Ddev <sams.alxstudent@gmail.com>
2023-06-18 07:38:27 -0700

    rework onntask 4 and 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,9 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
