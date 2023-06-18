a3ae5d3b95f5b616fe0162e2f5cd9f80104a952d
Sams-Ddev <sams.alxstudent@gmail.com>
2023-06-17 17:32:03 -0700

    assign a randon number to a variable n

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - assigning randome numbers to n
+ *
+ * Return - function zero
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 5)
+	{
+		printf("Last digit of n is %d and is greater than 5\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("Last digits of n is %d and is 0\n", n);
+	}
+	else if (n < 6 && n != 0)
+	{
+		printf("Last digit of n is %d and is less than 6 and not 0\n", n);
+	}
+	return (0);
+}
