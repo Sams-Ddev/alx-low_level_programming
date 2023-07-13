55d1280933740f77aa70e84981ae547db938e0b6
sams-ddev <sams.alxstudent@gmail.com>
2023-07-13 17:02:48 +0000

    wrote 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Write a function that allocates memory using malloc.
+ * @b: amount in bytes
+ *
+ * Return: pointer to the allocated memory.
+ * if malloc fails, the malloc_checked function should cause normal process
+ * termination with a status value of 98.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *b;
+
+	brslt = malloc(b);
+	if (brslt == NULL)
+		exit(98);
+	return (p);
+
+}
