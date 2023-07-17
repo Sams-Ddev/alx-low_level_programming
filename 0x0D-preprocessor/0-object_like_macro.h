d71c59c0f4fb037ac634df87ec93740c91062b84
sams-ddev <sams.alxstudent@gmail.com>
2023-07-17 18:36:21 +0000

    #ifndef definition for 0-object file

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,4 +2,9 @@
  * SIZE - a header file that defines a macro named SIZE
  * as an abbreviation for the token 1024.
  */
+#ifndef _OBJECT_LIKE_MACRO_
+#define _OBJECT_LIKE_MACRO_
+
 #define SIZE 1024
+
+#endif
