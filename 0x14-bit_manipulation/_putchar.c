49b2b34c6da480d91662817b1b0eb4494a318243
sams-ddev <sams.alxstudent@gmail.com>
2023-08-03 17:56:42 +0000

    wrote _putchar and 1-print_binary

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to the
+ * the standard output.
+ *
+ * @c: The character to print
+ *
+ * Return ;  On succeess 1.
+ * on error, _1 is returned, and error is set 
+ * appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
