56be5cb9ead3bc0ba9176a6ee5df1b1bb2bac53e
sams-ddev <sams.alxstudent@gmail.com>
2023-08-08 04:55:33 +0000

    rework on the 3-cp.c file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,80 +1,78 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include "main.h"
-
-#define BUFFER_SIZE 1024
-
-/**
- * copy_file - Copy the content of one file to another.
- * @file_from: Source file descriptor
- * @file_to: Destination file descriptor
- * Return: 0 on success, -1 on failure
- */
-
-int copy_file(int file_from, int file_to)
-{
-	ssize_t r_bytes, w_bytes;
-	char buffer[BUFFER_SIZE];
-
-	while ((r_bytes = read(file_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		w_bytes = write(file_to, buffer, r_bytes);
-		if (w_bytes == -1 || w_bytes != r_bytes)
-			return (-1);
-	}
-
-	return ((r_bytes == 0) ? 0 : -1);
-}
+#include <stdio.h>
 
 /**
- * main - Entry point
- * @argc: Number of arguments
- * @argv: Array of arguments
- * Return: 0 on success, various exit codes on failure
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
  */
-int main(int argc, char *argv[])
+
+void error_file(int file_from, int file_to, char *argv[])
 {
-	int file_from, file_to;
-
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-		return (EXIT_FAILURE);
-	}
-
-	file_from = open(argv[1], O_RDONLY);
-
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return (EXIT_FAILURE);
+		exit(98);
 	}
-
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(file_from);
-		return (EXIT_FAILURE);
+		exit(99);
 	}
+}
 
-	if (copy_file(file_from, file_to) == -1)
+/**
+ * main - check the code for ALX students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
+
+/* entry point for the 3-cp.c program*/
+int main(int argc, char *argv[])
+{
+	/* Variable declarations before use */
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
+	char buf[1024];
+
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(file_from);
-		close(file_to);
-		return (EXIT_FAILURE);
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
 	}
 
-	if (close(file_from) == -1 || close(file_to) == -1)
+	/* Open up the file (file_from) in READ only mode*/
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
+
+	nchars = 1024;
+	while (nchars == 1024)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (close(file_from) == -1) ? file_from : file_to);
-		return (EXIT_FAILURE);
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
 	}
 
-	return (EXIT_SUCCESS);
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
 }
 
