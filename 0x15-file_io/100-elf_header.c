f640aa1cff7e155b00b7e334527a6099beea9c30
sams-ddev <sams.alxstudent@gmail.com>
2023-08-08 04:35:08 +0000

    file requirements for 100-elf..

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,1 +1,53 @@
-#inlude
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <elf.h>
+
+void print_elf_header_info(Elf64_Ehdr *header) {
+    printf("Magic: ");
+    for (int i = 0; i < EI_NIDENT; i++) {
+        printf("%02x ", header->e_ident[i]);
+    }
+    printf("\nClass: %u\n", header->e_ident[EI_CLASS]);
+    printf("Data: %u\n", header->e_ident[EI_DATA]);
+    printf("Version: %u\n", header->e_ident[EI_VERSION]);
+    printf("OS/ABI: %u\n", header->e_ident[EI_OSABI]);
+    printf("ABI Version: %u\n", header->e_ident[EI_ABIVERSION]);
+    printf("Type: %u\n", header->e_type);
+    printf("Entry point address: %lx\n", header->e_entry);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+        return 98;
+    }
+
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        fprintf(stderr, "Error opening file: %s\n", argv[1]);
+        return 98;
+    }
+
+    Elf64_Ehdr header;
+    ssize_t bytes_read = read(fd, &header, sizeof(Elf64_Ehdr));
+    if (bytes_read != sizeof(Elf64_Ehdr)) {
+        fprintf(stderr, "Error reading ELF header\n");
+        close(fd);
+        return 98;
+    }
+
+    if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1] != ELFMAG1 ||
+        header.e_ident[EI_MAG2] != ELFMAG2 || header.e_ident[EI_MAG3] != ELFMAG3) {
+        fprintf(stderr, "Not an ELF file: %s\n", argv[1]);
+        close(fd);
+        return 98;
+    }
+
+    print_elf_header_info(&header);
+
+    close(fd);
+    return 0;
+}
+
