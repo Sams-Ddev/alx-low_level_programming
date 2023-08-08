a53c29df84addcc529f69c97eabab5ef30246449
sams-ddev <sams.alxstudent@gmail.com>
2023-08-08 05:19:51 +0000

    rework on advanced task

---
+++ 0x15-file_io/100_file.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <elf.h>
+
+/**
+ * print_elf_header_info - prints header infos
+ * main - entry point of function
+ *
+ * @header: header infomations handliners
+ */
+
+void print_elf_header_info(Elf64_Ehdr *header)
+{
+	int i;
+
+	printf("Magic: ");
+	for (i = 0; i < EI_NIDENT; i++)
+	{
+		printf("%02x ", header->e_ident[i]);
+	}
+
+	printf("\nClass: %u\n", header->e_ident[EI_CLASS]);
+	printf("Data: %u\n", header->e_ident[EI_DATA]);
+	printf("Version: %u\n", header->e_ident[EI_VERSION]);
+	printf("OS/ABI: %u\n", header->e_ident[EI_OSABI]);
+	printf("ABI Version: %u\n", header->e_ident[EI_ABIVERSION]);
+	printf("Type: %u\n", header->e_type);
+	printf("Entry point address: %lx\n", header->e_entry);
+}
+
+
+int main(int argc, char *argv[])
+{
+	int fd;
+	Elf64_Ehdr header;
+	 ssize_t bytes_read;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+		return (98);
+	}
+
+	fd = open(argv[1], O_RDONLY);
+
+	if (fd == -1)
+	{
+		fprintf(stderr, "Error opening file: %s\n", argv[1]);
+		return (98);
+	}
+
+	bytes_read = read(fd, &header, sizeof(Elf64_Ehdr));
+
+	if (bytes_read != sizeof(Elf64_Ehdr))
+	{
+		fprintf(stderr, "Error reading ELF header\n");
+		close(fd);
+		return (98);
+	}
+
+
+	if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1] != ELFMAG1 ||
+			header.e_ident[EI_MAG2] != ELFMAG2 || header.e_ident[EI_MAG3] != ELFMAG3)
+	{
+		fprintf(stderr, "Not an ELF file: %s\n", argv[1]);
+		close(fd);
+		return (98);
+	}
+
+	print_elf_header_info(&header);
+
+	close(fd);
+
+	return (0);
+}
+
