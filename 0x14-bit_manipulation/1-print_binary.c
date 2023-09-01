2c6d351ef4016386e3d177cc8601283e5db0d47a
sams-ddev <sams.alxstudent@gmail.com>
2023-09-01 01:10:59 +0000

    wrote _putchar

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,7 +3,13 @@
 /**
  * void print_binary - function that prints the binary representation
  * of a number.
- * @n:
- *
- * Return: 0 alwyas.
+ * @n: number to be printed in binary.
  */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
+
