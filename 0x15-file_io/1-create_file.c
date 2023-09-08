8b7a4ce78de24cb23eec3851c0c2d92cae9263ec
Samuel Zerd Ashigbui <sams.alxstudent@gmail.com>
2023-09-08 20:15:37 +0000

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include "main.h"
 
-int create_file(const char *filename, char *text_content) {
-    if (filename == NULL)
-        return -1;
+int create_file(const char *filename, char *text_content)
+{
+	int fd, x, y = 0;
 
-    int file_descriptor, write_result;
+	if (!filename)
+		return (-1);
 
-    /*Open or create the file with permissions rw-------*/
-    file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (file_descriptor == -1)
-        return -1;
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
 
-    /*If text_content is not NULL, write it to the file*/
-    if (text_content != NULL) {
-        write_result = write(file_descriptor, text_content, strlen(text_content));
-        if (write_result == -1) {
-            close(file_descriptor);
-            return -1;
-        }
-    }
+	if (text_content)
+	{
+		while (text_content[y])
+			y++;
+		x = write(fd, text_content, y);
+		if (x != y)
+			return (-1);
+	}
 
-    /* Close the file and return success (1)*/
-    close(file_descriptor);
-    return 1;
+	close(fd);
+
+	return (1);
 }
 
