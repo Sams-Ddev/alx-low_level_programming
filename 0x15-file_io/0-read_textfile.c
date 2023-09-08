6284d6e31543d44d5ae433e69fe8d5dbe11a777c
Samuel Zerd Ashigbui <sams.alxstudent@gmail.com>
2023-09-08 20:04:20 +0000

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include "main.h"
+
 /**
  * read_textfile - function that reads a text file and prints it to POSIX stdout.
  * @filename: filename.
@@ -11,44 +8,33 @@
  * Return:  actual number of letters it could read and print.
  */
 
-ssize_t read_textfile(const char *filename, size_t letters) {
-    if (filename == NULL)
-        return 0;
-
-    int file_descriptor, bytes_read, bytes_written;
-    char *buffer;
-
-    /* Open the file for reading*/
-    file_descriptor = open(filename, O_RDONLY);
-    if (file_descriptor == -1)
-        return 0;
-
-    /* Allocate memory for a buffer*/
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL) {
-        close(file_descriptor);
-        return 0;
-    }
-
-    /* Read from the file*/
-    bytes_read = read(file_descriptor, buffer, letters);
-    if (bytes_read == -1) {
-        close(file_descriptor);
-        free(buffer);
-        return 0;
-    }
-
-    /* Write to standard output */
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read) {
-        close(file_descriptor);
-        free(buffer);
-        return 0;
-    }
-
-    /* Clean up and return the actual number of letters read and printed */
-    close(file_descriptor);
-    free(buffer);
-    return bytes_written;
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	int i, y;
+	char *buf;
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+	i = read(fd, buf, letters);
+	if (i < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	buf[i] = '\0';
+	close(fd);
+	y = write(STDOUT_FILENO, buf, i);
+	if (y < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	return (y);
 }
-
