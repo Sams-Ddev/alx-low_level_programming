4b0e1de3226de5bf5d944bedec4c6efc4fc05a1b
Sams-Ddev <sams.alxstudent@gmail.com>
2023-09-22 20:21:22 +0000

    rework on 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,34 +1,29 @@
-#include "main.h"
-#include <stdio.h>
+
+#include<stdio.h>
+#include<stdarg.h>
+#include<stdio.h>
+
 /**
- * print_to_98 - Print all natural numbers from n to 98
- * @n: int type number
+ * sum_them_all - a function ...
+ * @n: the chaine
+ * @...: the char
+ *
+ * Return: 1 or 0
  */
-void print_to_98(int n)
+int sum_them_all(const unsigned int n, ...)
 {
-	if (n > 98)
-	{
-		while (n >= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n--;
-		}
-	}
-	else
-	{
-		while (n <= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n++;
-		}
-	}
-	printf("\n");
+	va_list ap;
+	unsigned int i, som = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		som = som + va_arg(ap, int);
+
+	va_end(ap);
+	return (som);
 }
+
