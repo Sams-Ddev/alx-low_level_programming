48eeeb212721c7453785563f61565147253fac02
Sams-Ddev <sams.alxstudent@gmail.com>
2023-09-29 04:34:36 +0000

    wrote 2-get_bit.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,11 @@
 #ifndef	_MAIN_H_
 #define	_MAIN_H_
 
+#include <stdlib.h>
+#include <stdio.h>
+
+/*----------my func() prototypes-------------*/
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
